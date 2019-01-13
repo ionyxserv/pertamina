b4d9a62329408d8646165dc41ab192f80beb2a62
ionyxserv <ahmad.amsori95@gmail.com>
2019-01-13 19:11:26 +0700

    Add login and tabs content

--- src/views/Base/Tabs/Tabs.js
+++ src/views/Base/Tabs/Tabs.js
@@ -1,6 +1,93 @@
 import React, {Component} from 'react';
-import {Badge, Col, Nav, NavItem, NavLink, Row, TabContent, TabPane} from 'reactstrap';
 import classnames from 'classnames';
+import { Bar, Line } from 'react-chartjs-2';
+import {
+  Badge,
+  Button,
+  ButtonDropdown,
+  ButtonGroup,
+  ButtonToolbar,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Col,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  NavItem,
+  NavLink,
+  Progress,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Row,
+  TabContent,
+  TabPane,
+  Table,
+} from 'reactstrap';
+import { getStyle } from '@coreui/coreui/dist/js/coreui-utilities'
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+
+const brandInfo = getStyle('--info')
+const cardChartData2 = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: brandInfo,
+      borderColor: 'rgba(255,255,255,.55)',
+      data: [1, 18, 9, 17, 34, 22, 11],
+    },
+  ],
+};
+const cardChartOpts2 = {
+  tooltips: {
+    enabled: false,
+    custom: CustomTooltips
+  },
+  maintainAspectRatio: false,
+  legend: {
+    display: false,
+  },
+  scales: {
+    xAxes: [
+      {
+        gridLines: {
+          color: 'transparent',
+          zeroLineColor: 'transparent',
+        },
+        ticks: {
+          fontSize: 2,
+          fontColor: 'transparent',
+        },
+
+      }],
+    yAxes: [
+      {
+        display: false,
+        ticks: {
+          display: false,
+          min: Math.min.apply(Math, cardChartData2.datasets[0].data) - 5,
+          max: Math.max.apply(Math, cardChartData2.datasets[0].data) + 5,
+        },
+      }],
+  },
+  elements: {
+    line: {
+      tension: 0.00001,
+      borderWidth: 1,
+    },
+    point: {
+      radius: 4,
+      hitRadius: 10,
+      hoverRadius: 4,
+    },
+  },
+};
 
 class Tabs extends Component {
 
@@ -17,6 +104,124 @@ class Tabs extends Component {
     return 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.'
   }
 
+  onlineEmployeeList(){
+    return(
+      <>
+        <Col xs="12" lg="12">
+          <Table responsive>
+            <thead>
+            <tr>
+              <th>Username</th>
+              <th>Date registered</th>
+              <th>Role</th>
+              <th>Status</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>Yiorgos Avraamu</td>
+              <td>2012/01/01</td>
+              <td>Member</td>
+              <td>
+                <Badge color="success">Active</Badge>
+              </td>
+            </tr>
+            <tr>
+              <td>Agapetus Tadeáš</td>
+              <td>2012/01/21</td>
+              <td>Staff</td>
+              <td>
+                <Badge color="success">Active</Badge>
+              </td>
+            </tr>
+            </tbody>
+          </Table>
+          <Pagination>
+            <PaginationItem disabled><PaginationLink previous tag="button">Prev</PaginationLink></PaginationItem>
+            <PaginationItem active>
+              <PaginationLink tag="button">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem><PaginationLink tag="button">2</PaginationLink></PaginationItem>
+            <PaginationItem><PaginationLink tag="button">3</PaginationLink></PaginationItem>
+            <PaginationItem><PaginationLink tag="button">4</PaginationLink></PaginationItem>
+            <PaginationItem><PaginationLink next tag="button">Next</PaginationLink></PaginationItem>
+          </Pagination>
+        </Col>
+      </>
+    );
+  }
+
+  allEmployeelist(){
+    return(
+      <>
+        <Col xs="12" lg="12">
+          <Table responsive>
+            <thead>
+            <tr>
+              <th>Username</th>
+              <th>Date registered</th>
+              <th>Role</th>
+              <th>Status</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>Yiorgos Avraamu</td>
+              <td>2012/01/01</td>
+              <td>Member</td>
+              <td>
+                <Badge color="success">Active</Badge>
+              </td>
+            </tr>
+            <tr>
+              <td>Avram Tarasios</td>
+              <td>2012/02/01</td>
+              <td>Staff</td>
+              <td>
+                <Badge color="secondary">Inactive</Badge>
+              </td>
+            </tr>
+            <tr>
+              <td>Quintin Ed</td>
+              <td>2012/02/01</td>
+              <td>Admin</td>
+              <td>
+                <Badge color="secondary">Inactive</Badge>
+              </td>
+            </tr>
+            <tr>
+              <td>Enéas Kwadwo</td>
+              <td>2012/03/01</td>
+              <td>Member</td>
+              <td>
+                <Badge color="secondary">Inactive</Badge>
+              </td>
+            </tr>
+            <tr>
+              <td>Agapetus Tadeáš</td>
+              <td>2012/01/21</td>
+              <td>Staff</td>
+              <td>
+                <Badge color="success">Active</Badge>
+              </td>
+            </tr>
+            </tbody>
+          </Table>
+          <Pagination>
+            <PaginationItem disabled><PaginationLink previous tag="button">Prev</PaginationLink></PaginationItem>
+            <PaginationItem active>
+              <PaginationLink tag="button">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem><PaginationLink tag="button">2</PaginationLink></PaginationItem>
+            <PaginationItem><PaginationLink tag="button">3</PaginationLink></PaginationItem>
+            <PaginationItem><PaginationLink tag="button">4</PaginationLink></PaginationItem>
+            <PaginationItem><PaginationLink next tag="button">Next</PaginationLink></PaginationItem>
+          </Pagination>
+        </Col>
+      </>
+    );
+  }
+
   toggle(tabPane, tab) {
     const newArray = this.state.activeTab.slice()
     newArray[tabPane] = tab
@@ -29,10 +234,10 @@ class Tabs extends Component {
     return (
       <>
         <TabPane tabId="1">
-          {`1. ${this.lorem()}`}
+          {this.onlineEmployeeList()}
         </TabPane>
         <TabPane tabId="2">
-          {`2. ${this.lorem()}`}
+          {this.allEmployeelist()}
         </TabPane>
         <TabPane tabId="3">
           {`3. ${this.lorem()}`}
@@ -45,101 +250,20 @@ class Tabs extends Component {
     return (
       <div className="animated fadeIn">
         <Row>
-          <Col xs="12" md="6" className="mb-4">
-            <Nav tabs>
-              <NavItem>
-                <NavLink
-                  active={this.state.activeTab[0] === '1'}
-                  onClick={() => { this.toggle(0, '1'); }}
-                >
-                  Home
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  active={this.state.activeTab[0] === '2'}
-                  onClick={() => { this.toggle(0, '2'); }}
-                >
-                  Profile
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  active={this.state.activeTab[0] === '3'}
-                  onClick={() => { this.toggle(0, '3'); }}
-                >
-                  Messages
-                </NavLink>
-              </NavItem>
-            </Nav>
-            <TabContent activeTab={this.state.activeTab[0]}>
-              {this.tabPane()}
-            </TabContent>
+          <Col xs="12" md="12">
+            <Card className="text-white bg-info">
+              <CardBody className="pb-0">
+                <div className="text-value">9.823</div>
+                <div>Employee online</div>
+              </CardBody>
+              <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
+                <Line data={cardChartData2} options={cardChartOpts2} height={70} />
+              </div>
+            </Card>
           </Col>
-          <Col xs="12" md="6" className="mb-4">
-            <Nav tabs>
-              <NavItem>
-                <NavLink
-                  active={this.state.activeTab[1] === '1'}
-                  onClick={() => { this.toggle(1, '1'); }}
-                >
-                  <i className="icon-calculator"></i>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  active={this.state.activeTab[1] === '2'}
-                  onClick={() => { this.toggle(1, '2'); }}
-                >
-                  <i className="icon-basket-loaded"></i>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  active={this.state.activeTab[1] === '3'}
-                  onClick={() => { this.toggle(1, '3'); }}
-                >
-                  <i className="icon-pie-chart"></i>
-                </NavLink>
-              </NavItem>
-            </Nav>
-            <TabContent activeTab={this.state.activeTab[1]}>
-              {this.tabPane()}
-              </TabContent>
-          </Col>
-          <Col xs="12" md="6" className="mb-4">
-            <Nav tabs>
-              <NavItem>
-                <NavLink
-                  active={this.state.activeTab[2] === '1'}
-                  onClick={() => { this.toggle(2, '1'); }}
-                >
-                  <i className="icon-calculator"></i> <span className={this.state.activeTab[2] === '1' ? '' : 'd-none'}> Calculator</span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  active={this.state.activeTab[2] === '2'}
-                  onClick={() => { this.toggle(2, '2'); }}
-                >
-                  <i className="icon-basket-loaded"></i> <span
-                  className={this.state.activeTab[2] === '2' ? '' : 'd-none'}> Shopping cart</span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  className={classnames({ active: this.state.activeTab[2] === '3' })}
-                  onClick={() => { this.toggle(2,'3'); }}
-                >
-                  <i className="icon-pie-chart"></i> <span className={this.state.activeTab[2] === '3' ? '' : 'd-none'}> Charts</span>
-                </NavLink>
-              </NavItem>
-            </Nav>
-            <TabContent activeTab={this.state.activeTab[2]}>
-              {this.tabPane()}
-            </TabContent>
-          </Col>
-          <Col xs="12" md="6" className="mb-4">
+        </Row>
+        <Row>
+          <Col xs="12" md="12" className="mb-4">
             <Nav tabs>
               <NavItem>
                 <NavLink
@@ -147,8 +271,8 @@ class Tabs extends Component {
                   onClick={() => { this.toggle(3, '1'); }}
                 >
                   <i className="icon-calculator"></i>
-                  <span className={this.state.activeTab[3] === '1' ? '' : 'd-none'}> Calc</span>
-                  {'\u00A0'}<Badge color="success">New</Badge>
+                  <span className={this.state.activeTab[3] === '1' ? '' : 'd-none'}> Online</span>
+                  {'\u00A0'}<Badge color="success">2</Badge>
                 </NavLink>
               </NavItem>
               <NavItem>
@@ -157,8 +281,8 @@ class Tabs extends Component {
                   onClick={() => { this.toggle(3, '2'); }}
                 >
                   <i className="icon-basket-loaded"></i>
-                  <span className={this.state.activeTab[3] === '2' ? '' : 'd-none'}> Cart</span>
-                  {'\u00A0'}<Badge pill color="danger">29</Badge>
+                  <span className={this.state.activeTab[3] === '2' ? '' : 'd-none'}> All Employee</span>
+                  {'\u00A0'}<Badge pill color="Light">29</Badge>
                 </NavLink>
               </NavItem>
               <NavItem>
--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -35,9 +35,6 @@ class Login extends Component {
                         <Col xs="6">
                           <Button color="primary" className="px-4">Login</Button>
                         </Col>
-                        <Col xs="6" className="text-right">
-                          <Button color="link" className="px-0">Forgot password?</Button>
-                        </Col>
                       </Row>
                     </Form>
                   </CardBody>
@@ -45,12 +42,7 @@ class Login extends Component {
                 <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
                   <CardBody className="text-center">
                     <div>
-                      <h2>Sign up</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                      <Link to="/register">
-                        <Button color="primary" className="mt-3" active tabIndex={-1}>Register Now!</Button>
-                      </Link>
+                      <img src={'https://1.bp.blogspot.com/-A0B_VNhNwdg/WvVFtXobm_I/AAAAAAAAB8I/yjjq2LE60IEWh6d25JhJh20Btd5cTDHhQCLcBGAs/s1600/logo%2Bpertamina.png'} className="img-avatar" alt="admin@bootstrapmaster.com" />
                     </div>
                   </CardBody>
                 </Card>
